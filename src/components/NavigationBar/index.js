d9a605df759cb11d84a9a78636ef6e45c8931fa0
Thong Nguyen <thongnguyen@pixelcent.com>
2022-12-26 22:03:10 +0700

    Fix draft nav foot

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -4,6 +4,8 @@ import CustomDropdown from "components/Commons/CustomDropdown";
 
 import classes from "./styles.module.scss";
 
+import { Link } from 'react-router-dom';
+
 const NavigationBar = ( {user_role, username, current_route } ) => {
     
     const [ isUserNameClicked, setUserNameClicked ] = useState(false)
@@ -13,9 +15,9 @@ const NavigationBar = ( {user_role, username, current_route } ) => {
             <img src = "logo.png" alt = "logo" width = '50px'/>
 
             <div className = {classes.navigator}>
-                { (user_role === 'Trainer' && current_route === 'My post') ? <div className = {classes.currentRoute}> My post </div> : ((user_role === 'Trainer') && <div> My post </div>) }
-                { (current_route === 'Favorites') ? <div className = {classes.currentRoute}> Marketplace </div> : <div> Favorites </div>}
-                { (current_route === 'Marketplace') ? <div className = {classes.currentRoute}> Marketplace </div> : <div> Marketplace </div>}
+                { (user_role === 'Trainer' && current_route === 'My post') ? <div className = {classes.currentRoute}><Link to="">My post</Link></div> : ((user_role === 'Trainer') && <div className={classes.route}> My post </div>) }
+                { (current_route === 'Favorites') ? <div className = {classes.currentRoute}> Marketplace </div> : <div className={classes.route}> Favorites </div>}
+                { (current_route === 'Marketplace') ? <div className = {classes.currentRoute}> Marketplace </div> : <div className={classes.route}> Marketplace </div>}
             </div>
 
             <div style={{position: 'relative'}}> 
