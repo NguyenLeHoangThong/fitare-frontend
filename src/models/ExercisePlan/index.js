ab6a9ca038eb53da6ddb613dc48b4f31c0f5718f
Thong Nguyen <thongnguyen@pixelcent.com>
2022-12-28 21:24:36 +0700

    Add models, exerciseplan service

---
+++ src/models/ExercisePlan/index.js
@@ -0,0 +1,101 @@
+export const bmiTypes = [
+    {
+        id: 1,
+        description: "< 18.5",
+        name: "Underweight",
+        value: "UNDER_WEIGHT"
+    },
+    {
+        id: 2,
+        description: "18.5 - 24.9",
+        name: "Normal weight",
+        value: "NORMAL_WEIGHT"
+    },
+    {
+        id: 3,
+        description: "25 - 29.9",
+        name: "Overweight",
+        value: "OVER_WEIGHT"
+    },
+    {
+        id: 4,
+        description: ">30",
+        name: "Obesity",
+        value: "OBESITY"
+    },
+];
+
+export const muscleGroupTypes = [
+    {
+        id: 1,
+        name: "Arms",
+        value: "ARMS"
+    },
+    {
+        id: 2,
+        name: "Chest",
+        value: "CHEST"
+    },
+    {
+        id: 3,
+        name: "Abdomen",
+        value: "ABDOMEN"
+    },
+    {
+        id: 4,
+        name: "Shoulders",
+        value: "SHOULDERS"
+    },
+    {
+        id: 5,
+        name: "Legs",
+        value: "LEGS"
+    }
+];
+
+export const durationTypes = [
+    {
+        id: 1,
+        name: "15 - 30 minutes",
+        value: "15_30"
+    },
+    {
+        id: 2,
+        name: "30 - 60 minutes",
+        value: "30_60"
+    },
+    {
+        id: 3,
+        name: "60 - 90 minutes",
+        value: "60_90"
+    }
+];
+
+
+export const levelTypes = [
+    {
+        id: 1,
+        name: "1",
+        value: "BEGINNER"
+    },
+    {
+        id: 2,
+        name: "2",
+        value: "INTERMEDIATE"
+    },
+    {
+        id: 3,
+        name: "3",
+        value: "ADVANCED"
+    },
+    {
+        id: 3,
+        name: "4",
+        value: "HARD"
+    },
+    {
+        id: 3,
+        name: "5",
+        value: "EXTREME"
+    }
+];
