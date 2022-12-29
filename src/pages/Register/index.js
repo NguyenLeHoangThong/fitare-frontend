3e13d9f761f32b14ae312f14a34a9fa751e89826
UnDeadCharger <rexfury121@gmail.com>
2022-12-29 21:29:31 +0700

    Added Register Form

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -11,6 +11,11 @@ import classes from "./styles.module.scss";
 import { setLoading } from "redux/reducers/Status/actionTypes";
 import { FaEnvelope } from 'react-icons/fa';
 import { FaLock } from "react-icons/fa";
+import { FaUserAlt } from "react-icons/fa";
+
+import { Link } from 'react-router-dom';
+import { routes } from 'routers/routes.js';
+
 const Register = memo((props) => {
 
     const dispatch = useDispatch()
@@ -18,7 +23,10 @@ const Register = memo((props) => {
     const schema = useMemo(() => {
         return yup.object().shape({
             email: yup.string().email("Please input valid email").required("Please input email"),
-            password: yup.string().required("Please input password")
+            password: yup.string().required("Please input password"),
+            conPassword: yup.string().oneOf([yup.ref('password'), null], "Passwords must match"),
+            firstName: yup.string().required("Please input First Name"),
+            lastName: yup.string().required("Please input Last Name"),
         })
     }, []);
 
@@ -48,41 +56,74 @@ const Register = memo((props) => {
             <div className={classes.title}>SIGN UP</div>
 
             <Form onSubmit={handleSubmit(onSubmit)} >
-                <div className={classes.flexInput}> 
-                    <FaEnvelope className={classes.icon} />
-                    <CustomInput
-                        inputRef="email"
-                        className={classes.textboxInput}
-                        placeholder="email"
-                        control={control}
-                        errorMessage={errors?.email?.message}
-                    />
+                <div className={classes.flexBox}>
+                    <div className={classes.Column}>
+                        <div className={classes.flexInput}>
+                            <FaEnvelope className={classes.icon} />
+                            <CustomInput
+                                inputRef="email"
+                                className={classes.textboxInput}
+                                placeholder="email"
+                                control={control}
+                                errorMessage={errors?.email?.message}
+                            />
+                        </div>
+                        <div className={classes.flexInput}>
+                            <FaLock className={classes.icon} />
+                            <CustomInput
+                                inputRef="password"
+                                className={classes.textboxInput}
+                                placeholder="password"
+                                control={control}
+                                errorMessage={errors?.password?.message}
+                                isPassword 
+                            />
+                        </div>
+                        <div className={classes.flexInput}>
+                            <FaLock className={classes.icon} />
+                            <CustomInput
+                                inputRef="conPassword"
+                                className={classes.textboxInput}
+                                placeholder="confirm password"
+                                control={control}
+                                errorMessage={errors?.conPassword?.message}
+                                isPassword 
+                            />
+                        </div>
+                    </div>
+                    <div className={classes.Column}>
+                        <div className={classes.flexInput}>
+                            <FaUserAlt className={classes.icon} />
+                            <CustomInput
+                                inputRef="firstName"
+                                className={classes.textboxInput}
+                                placeholder="First Name"
+                                control={control}
+                                errorMessage={errors?.email?.message}
+                                
+                            />
+                        </div>
+                        <div className={classes.flexInput}>
+                            <FaUserAlt className={classes.icon} />
+                            <CustomInput
+                                inputRef="lastName"
+                                className={classes.textboxInput}
+                                placeholder="Last Name"
+                                control={control}
+                                errorMessage={errors?.email?.message}
+                                
+                            />
+                        </div>
+
+                    </div>
+
                 </div>
-                <div className={classes.flexInput}> 
-                <FaLock className={classes.icon} />
-                <CustomInput
-                    inputRef="password"
-                    className={classes.textboxInput}
-                    placeholder="password"
-                    control={control}
-                    errorMessage={errors?.password?.message}
-                />
-                </div>
-                <div className={classes.flexInput}> 
-                <FaLock className={classes.icon} />
-                <CustomInput
-                    inputRef="password"
-                    className={classes.textboxInput}
-                    placeholder="confirm password"
-                    control={control}
-                    errorMessage={errors?.password?.message}
-                />
-                </div>
+
                 <div className={classes.flexContent}>
-                    <Button className={classes.btnRegister}>Sign Up</Button>
-                    <Button type="submit" className={classes.btnLogin}>Sign In</Button>
+                    <Button className={classes.btnLogin}><Link to={routes.login} className={classes.noDecor}>Sign In</Link></Button>
+                    <Button type="submit" className={classes.btnRegister}>Sign Up</Button>
                 </div>
-                
+
             </Form>
         </div>
     )
