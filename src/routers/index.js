82d91bacbe812addaa7baea3227ebf50ca885689
nguyenthienphu1206 <79633694+nguyenthienphu1206@users.noreply.github.com>
2022-12-30 01:42:55 +0700

    Update Edit Exercise Plan and Edit Exercise Screen

--- src/routers/index.js
+++ src/routers/index.js
@@ -10,6 +10,9 @@ import SetContent from "pages/SetContent";
 import Plans from "pages/Plans";
 import MyPlans from "pages/MyPlans";
 import SetExercisePlan from "pages/SetExercisePlan";
+import SetExercise from "pages/SetExercise";
+import EditExercisePlan from "pages/EditExercisePlan";
+import EditExercise from "pages/EditExercise";
 import TraineeAuthRoute from "./TraineeAuthRoute";
 
 const Routers = () =>   {
@@ -23,6 +26,9 @@ const Routers = () =>   {
                 <Route path={routes.plans} exact render={(props) => <Plans {...props} />} />
                 <Route path={routes.myPlans} exact render={(props) => <MyPlans {...props} />} />
                 <Route path={routes.setExercisePlan} exact render={(props) => <SetExercisePlan {...props} />} />
+                <Route path={routes.setExercise} exact render={(props) => <SetExercise {...props} />} />
+                <Route path={routes.editExercisePlan} exact render={(props) => <EditExercisePlan {...props} />} />
+                <Route path={routes.editExercise} exact render={(props) => <EditExercise {...props} />} />
                 
                 <Route path={routes.setSummary} render={(props) => <SetSummary {...props} />} />
                 <Route path={routes.setContent} exact render={(props) => <SetContent {...props} />} />
